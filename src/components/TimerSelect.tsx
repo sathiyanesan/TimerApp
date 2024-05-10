4b035797dcaeb7bebe27d7269561b4df5f8dd8b3
sathiyanesan <sathiyanesan95@gmail.com>
2024-05-10 13:05:51 +0100

    Added snack bar message when no time selection

--- src/components/TimerSelect.tsx
+++ src/components/TimerSelect.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import dayjs from 'dayjs';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -6,45 +6,55 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MultiSectionDigitalClock } from '@mui/x-date-pickers/MultiSectionDigitalClock';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import DigitalClockDisplay from "./DigitalClockDisplay";
+import Snackbar from '@mui/material/Snackbar';
 
 
 const TimerSelect = ({ openClock, setOpenClock, setOpenAlert }: any) => {
     const [timerValue, setTimerValue] = React.useState<dayjs.Dayjs | null>(dayjs('2022-04-17T00:00:00'));
     // const [openClock, setOpenClock] = useState(false);
+    const [openMsg, setOpenMsg] = useState(false);
 
 
     const noSelection: boolean = timerValue?.get('hour') === 0 && timerValue?.get('minute') === 0 && timerValue?.get('second') === 0;
 
     return (
-        <>{!openClock ? (
-            <div>
-                <div className="app-title">
-                    <p className="title-text">"Countdown to Your Dreams: Let Our App Keep You on Track!"</p>
-                </div>
+        <>
+            {!openClock ? (
                 <div>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DemoContainer
-                            components={['MobileTimePicker', 'MultiSectionDigitalClock']}
-                        >
-                            <DemoItem>
-                                <MultiSectionDigitalClock
-                                    value={timerValue}
-                                    onChange={(newValue) => { setTimerValue(newValue) }}
-                                    views={['hours', 'minutes', 'seconds']}
-                                    ampm={false}
-                                    timeSteps={{ hours: 1, minutes: 1, seconds: 1 }}
-                                />
-                            </DemoItem>
-                        </DemoContainer>
-                    </LocalizationProvider>
+                    {<Snackbar
+                        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                        open={openMsg}
+                        autoHideDuration={3000}
+                        onClose={() => { setOpenMsg(false) }}
+                        message="Please select time (hours or minutes or seconds)."
+                    />}
+                    <div className="app-title">
+                        <p className="title-text">"Countdown to Your Dreams: Let Our App Keep You on Track!"</p>
+                    </div>
+                    <div>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DemoContainer
+                                components={['MobileTimePicker', 'MultiSectionDigitalClock']}
+                            >
+                                <DemoItem>
+                                    <MultiSectionDigitalClock
+                                        value={timerValue}
+                                        onChange={(newValue) => { setTimerValue(newValue) }}
+                                        views={['hours', 'minutes', 'seconds']}
+                                        ampm={false}
+                                        timeSteps={{ hours: 1, minutes: 1, seconds: 1 }}
+                                    />
+                                </DemoItem>
+                            </DemoContainer>
+                        </LocalizationProvider>
+                    </div>
                 </div>
-            </div>
-        )
-            : (<DigitalClockDisplay
-                timers={timerValue}
-                setOpenClock={setOpenClock}
-                setOpenAlert={setOpenAlert}
-            />)}
+            )
+                : (<DigitalClockDisplay
+                    timers={timerValue}
+                    setOpenClock={setOpenClock}
+                    setOpenAlert={setOpenAlert}
+                />)}
 
             <div>
                 {!openClock &&
@@ -58,7 +68,10 @@ const TimerSelect = ({ openClock, setOpenClock, setOpenAlert }: any) => {
                                 color: noSelection ? "#C8AE7D" : "#7A9D54"
                             }
                         }}
-                        onClick={() => { !noSelection && setOpenClock(true) }}
+                        onClick={() => {
+                            !noSelection && setOpenClock(true)
+                            noSelection && setOpenMsg(true)
+                        }}
                     />
                 }
             </div>
